833a5c83337b2d3022ecf4c9557a85680c63fdd2
XukunXiang <xxk08300190024@gmail.com>
2021-10-25 22:17:50 -0500

    Container Orchestration, fail to pull docker image

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2e42f329a5ba41b9b9ebd52b9a829be-2048555126.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
